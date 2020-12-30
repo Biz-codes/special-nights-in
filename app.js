806c7b7f94dff1a2f271813aaa561687eefc24d2
Biz Hight <elizabeth.biz.hight@gmail.com>
2020-12-29 20:29:26 -0500

    commented out console.logs

--- app.js
+++ app.js
@@ -154,78 +154,24 @@ function formatQueryParams(params) {
 }
 
 function displayCocktailRecipes(responseJson) {
-    console.log(responseJson);
+    // console.log(responseJson);
     $('#cocktail-results').empty();
-
-
-    // dateModified: "2015-08-18 14:42:59"
-    // idDrink: "11007"
-    // strAlcoholic: "Alcoholic"
-    // strCategory: "Ordinary Drink"
-    // strCreativeCommonsConfirmed: "Yes"
-    // strDrink: "Margarita"
-    // strDrinkAlternate: null
-    // strDrinkDE: null
-    // strDrinkES: null
-    // strDrinkFR: null
-    // strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
-    // strDrinkZH-HANS: null
-    // strDrinkZH-HANT: null
-    // strGlass: "Cocktail glass"
-    // strIBA: "Contemporary Classics"
-    // strImageAttribution: "Cocktailmarler"
-    // strImageSource: "https://commons.wikimedia.org/wiki/File:Klassiche_Margarita.jpg"
-    // strIngredient1: "Tequila"
-    // strIngredient2: "Triple sec"
-    // strIngredient3: "Lime juice"
-    // strIngredient4: "Salt"
-    // strIngredient5: null
-    // strIngredient6: null
-    // strIngredient7: null
-    // strIngredient8: null
-    // strIngredient9: null
-    // strIngredient10: null
-    // strIngredient11: null
-    // strIngredient12: null
-    // strIngredient13: null
-    // strIngredient14: null
-    // strIngredient15: null
-    // strInstructions: "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass."
-    // strInstructionsDE: "Reiben Sie den Rand des Glases mit der Limettenscheibe, damit das Salz daran haftet. Achten Sie darauf, dass nur der äußere Rand angefeuchtet wird und streuen Sie das Salz darauf. Das Salz sollte sich auf den Lippen des Genießers befinden und niemals in den Cocktail einmischen. Die anderen Zutaten mit Eis schütteln und vorsichtig in das Glas geben."
-    // strInstructionsES: null
-    // strInstructionsFR: null
-    // strInstructionsZH-HANS: null
-    // strInstructionsZH-HANT: null
-    // strMeasure1: "1 1/2 oz "
-    // strMeasure2: "1/2 oz "
-    // strMeasure3: "1 oz "
-    // strMeasure4: null
-    // strMeasure5: null
-    // strMeasure6: null
-    // strMeasure7: null
-    // strMeasure8: null
-    // strMeasure9: null
-    // strMeasure10: null
-    // strMeasure11: null
-    // strMeasure12: null
-    // strMeasure13: null
-    // strMeasure14: null
-    // strMeasure15: null
-    // strTags: "IBA,ContemporaryClassic"
-    // strVideo: null
-
-
-if(responseJson.drinks.length == 0) { 
-    alert("no results")
-}
-else {
-    // iterate through the data array
-    for (let i = 0; i < responseJson.drinks.length; i++) {
-        // for each recipe object in the cocktails 
-        //array, add a list item to the results 
-        //with thumbnail
+    if (responseJson.drinks == null) {
         $('#cocktail-results').append(
             `<li class="recipe">
+            <h4>&#127864;  No results:</h4>
+            <p>Sorry, we couldn't find any cocktail matches. Please check your spelling or try a new search.</p>
+        </li>`
+        )
+    } 
+    else {
+        // iterate through the data array
+        for (let i = 0; i < responseJson.drinks.length; i++) {
+            // for each recipe object in the cocktails 
+            //array, add a list item to the results 
+            //with thumbnail
+            $('#cocktail-results').append(
+                `<li class="recipe">
                 <h4>&#127864;  ${responseJson.drinks[i].strDrink}</h4>
                 <img src='${responseJson.drinks[i].strDrinkThumb}' class='thumb'>
                 <ul>
@@ -242,7 +188,8 @@ else {
                 </ul>
                 <p>${responseJson.drinks[i].strInstructions}</p>
             </li>`
-        )}
+            )
+        }
     };
     //display the results section  
     $('#cocktail-heading').removeClass('hidden');
@@ -251,28 +198,33 @@ else {
 
 
 function displayMealRecipes(responseJson) {
-    console.log(responseJson);
+    // console.log(responseJson);
     $('#meal-results').empty();
     // iterate through the meal array
-    if(responseJson.meals.length == 0) {
-        alert("no results")
-    }
-    else {
-    for (let i = 0; i < responseJson.meals.length; i++) {
-        // for each recipe object in the meals 
-        //array, add a list item to the results 
-        //with thumbnail
-        // if (responseJson.meals=="null"){
-        //      $('#meal-error-msg').append(
-        //          `<p>Sorry, we couldn't find any meal matches. Please check your spelling or try a new search.</p>`
-        //      )
-        //  }
-        //  else
-//127864 cocktail emoji  127858 meal emoji
-
-
+    if (responseJson.meals == null) {
         $('#meal-results').append(
             `<li class="recipe">
+            <h4>&#127858;  No results:</h4>
+            <p>Sorry, we couldn't find any meal matches. Please check your spelling or try a new search.</p>
+        </li>`
+        )
+    } 
+    else {
+        for (let i = 0; i < responseJson.meals.length; i++) {
+            // for each recipe object in the meals 
+            //array, add a list item to the results 
+            //with thumbnail
+            // if (responseJson.meals=="null"){
+            //      $('#meal-error-msg').append(
+            //          `<p>Sorry, we couldn't find any meal matches. Please check your spelling or try a new search.</p>`
+            //      )
+            //  }
+            //  else
+            //127864 cocktail emoji  127858 meal emoji
+
+
+            $('#meal-results').append(
+                `<li class="recipe">
                 <h4>&#127858;  ${responseJson.meals[i].strMeal}</h4>
                 <img src='${responseJson.meals[i].strMealThumb}' class='thumb'>
                 <ul>
@@ -289,79 +241,27 @@ function displayMealRecipes(responseJson) {
                 </ul>
                 <p>${responseJson.meals[i].strInstructions}</p>
             </li>`
-        )}
+            )
+        }
     };
     //display the results section  
     $('#meal-heading').removeClass('hidden');
     $('#meal-results').removeClass('hidden');
 }
 
- // "idMeal": "52771",
-    // "strMeal": "Spicy Arrabiata Penne",
-    // "strDrinkAlternate": null,
-    // "strCategory": "Vegetarian",
-    // "strArea": "Italian",
-    // "strInstructions": "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
-    // "strMealThumb": "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-    // "strTags": "Pasta,Curry",
-    // "strYoutube": "https://www.youtube.com/watch?v=1IszT_guI08",
-    // "strIngredient1": "penne rigate",
-    // "strIngredient2": "olive oil",
-    // "strIngredient3": "garlic",
-    // "strIngredient4": "chopped tomatoes",
-    // "strIngredient5": "red chile flakes",
-    // "strIngredient6": "italian seasoning",
-    // "strIngredient7": "basil",
-    // "strIngredient8": "Parmigiano-Reggiano",
-    // "strIngredient9": "",
-    // "strIngredient10": "",
-    // "strIngredient11": "",
-    // "strIngredient12": "",
-    // "strIngredient13": "",
-    // "strIngredient14": "",
-    // "strIngredient15": "",
-    // "strIngredient16": null,
-    // "strIngredient17": null,
-    // "strIngredient18": null,
-    // "strIngredient19": null,
-    // "strIngredient20": null,
-    // "strMeasure1": "1 pound",
-    // "strMeasure2": "1/4 cup",
-    // "strMeasure3": "3 cloves",
-    // "strMeasure4": "1 tin ",
-    // "strMeasure5": "1/2 teaspoon",
-    // "strMeasure6": "1/2 teaspoon",
-    // "strMeasure7": "6 leaves",
-    // "strMeasure8": "spinkling",
-    // "strMeasure9": "",
-    // "strMeasure10": "",
-    // "strMeasure11": "",
-    // "strMeasure12": "",
-    // "strMeasure13": "",
-    // "strMeasure14": "",
-    // "strMeasure15": "",
-    // "strMeasure16": null,
-    // "strMeasure17": null,
-    // "strMeasure18": null,
-    // "strMeasure19": null,
-    // "strMeasure20": null,
-    // "strSource": null,
-    // "dateModified": null
-
-
 function getCocktails(searchTerm) {
-    console.log(searchTerm);
+    // console.log(searchTerm);
     const params = {
         s: searchTerm,
     };
     const queryString = formatQueryParams(params);
     const url = cocktailSearchURL + '?' + queryString;
 
-    console.log(url)
+    //console.log(url)
 
     fetch(url)
         .then(response => {
-            console.log(response);
+            // console.log(response);
             if (response.ok) {
                 return response.json();
             }
@@ -374,18 +274,18 @@ function getCocktails(searchTerm) {
 }
 
 function getMeals(searchTerm) {
-    console.log(searchTerm);
+    //console.log(searchTerm);
     const params = {
         s: searchTerm,
     };
     const queryString = formatQueryParams(params);
     const url = mealSearchURL + '?' + queryString;
 
-    console.log(url)
+    // console.log(url)
 
     fetch(url)
         .then(response => {
-            console.log(response);
+            // console.log(response);
             if (response.ok) {
                 return response.json();
             }
@@ -409,4 +309,3 @@ function startUp() {
 }
 
 $(startUp)
-
